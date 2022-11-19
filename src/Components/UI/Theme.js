8cd858e0089ff1f441ed277a22b5b922eace831f
Shashwat Gangwar <89464765+FrolicBrat@users.noreply.github.com>
2022-11-19 15:22:43 +0530

    Initial commit

---
+++ src/Components/UI/Theme.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Theme = () => {
+  return (
+    <div className="center" style={{ margin: "30px" }}>
+      <iframe
+        title="myFrame"
+        src="https://open.spotify.com/embed/track/2hsLpiKNkWpd4e9QuVdhar"
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Theme;
